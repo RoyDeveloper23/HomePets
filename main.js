1d9fb7f38583fae6a987f6c9b38967b3b788392b
RoyDeveloper23 <royerazo23@gmail.com>
2024-07-16 23:57:16 -0500

    HomePets terminado

---
+++ main.js
@@ -0,0 +1,18 @@
+const typed = new Typed(".typed", {
+  strings: [
+    "<i class='mascota'>Gato<i>",
+    "<i class='mascota'>Perro<i>",
+    "<i class='mascota'>Conejo<i>",
+    "<i class='mascota'>Pez<i>",
+  ],
+  typeSpeed: 75,
+  starDelay: 500,
+  backSpeed: 75,
+  shuffle: false,
+  backDelay: 1000,
+  showCursor: true,
+  cursorChar: "|",
+  contentType: "html",
+  loopCount: false,
+  loop: true,
+});
